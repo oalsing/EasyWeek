6847160fc8ce3d1d5faa3baa2251440fc2bab2c8
oalsing <oscar.alsing@gmail.com>
2016-02-23 19:15:13 +0100

    Initial Commit

---
+++ EasyWeek/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  EasyWeek
+//
+//  Created by Oscar Alsing on 23/02/16.
+//  Copyright © 2016 Oscar Alsing. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
